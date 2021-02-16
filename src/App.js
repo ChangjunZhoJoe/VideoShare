d986ee3cea0af21d23b40ffb81f6c22d05b40c13
joe <421613663@qq.com>
2021-02-16 09:20:57 -0500

    test

--- src/App.js
+++ src/App.js
@@ -3,33 +3,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
-import { DataStore } from '@aws-amplify/datastore';
-import { Video } from './models';
 
 import Button from '@material-ui/core/Button';
-
-function addVideo(){
-  DataStore.save(
-    new Video({
-    "videosessionID": uuidv4(),
-    "StorageLocation": "blabla"
-    })
-  ).then(()=>{
-    getAllVideos()
-  })
-}
-
-function getAllVideos(){
-  DataStore.query(Video).then((v)=>{
-    console.log(v)
-  })
-}
-
-// function deleteAllVideos(){
-//   DataStore.delete(Video, Predicates.ALL).then(()=>{
-//     alert("deleted all videos");
-//   })
-// }
 
 function App() {
   
@@ -40,9 +15,6 @@ function App() {
         <p>
           Welcome to Changjun Zhou's website!
         </p>
-        <Button onClick ={ ()=>{addVideo()} } variant="contained" color="primary" href="#contained-buttons">
-          Add Video
-        </Button>
       </header>
     </div>
   );
